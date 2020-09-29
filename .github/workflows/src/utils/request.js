7b9337a5e4b8997c144e1600b9b8ee2562f46f5b
vchiapaikeo <vchiapaikeo@etsy.com>
2020-09-29 23:04:36 +0000

    More stuff

---
+++ .github/workflows/src/utils/request.js
@@ -0,0 +1,37 @@
+const https = require("https");
+
+/**
+ * Helper function for making HTTP requests
+ * @param {string | URL} url - Request URL
+ * @param {object} options - Request options
+ * @returns {Promise<object>} - JSON response
+ */
+function request(url, options) {
+  return new Promise((resolve, reject) => {
+    const req = https
+      .request(url, options, (res) => {
+        let data = "";
+        res.on("data", (chunk) => {
+          data += chunk;
+        });
+        res.on("end", () => {
+          if (res.statusCode >= 400) {
+            const err = new Error(`Received status code ${res.statusCode}`);
+            err.response = res;
+            err.data = data;
+            reject(err);
+          } else {
+            resolve({ res, data: JSON.parse(data) });
+          }
+        });
+      })
+      .on("error", reject);
+    if (options.body) {
+      req.end(JSON.stringify(options.body));
+    } else {
+      req.end();
+    }
+  });
+}
+
+module.exports = request;
---
+++ .github/workflows/src/utils/string.js
@@ -0,0 +1,22 @@
+/**
+ * Capitalizes the first letter of a string
+ * @param {string} str - String to process
+ * @returns {string} - Input string with first letter capitalized
+ */
+function capitalizeFirstLetter(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+/**
+ * Removes the trailing period from the provided string (if it has one)
+ * @param {string} str - String to process
+ * @returns {string} - String without trailing period
+ */
+function removeTrailingPeriod(str) {
+  return str[str.length - 1] === "." ? str.substring(0, str.length - 1) : str;
+}
+
+module.exports = {
+  capitalizeFirstLetter,
+  removeTrailingPeriod,
+};
